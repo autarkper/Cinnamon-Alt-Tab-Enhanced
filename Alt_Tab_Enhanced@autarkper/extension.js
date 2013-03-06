a379790bc24eae8d3c51126c65f266591e23844d
Per Ångström <github@autark.se>
2013-03-06 08:59:32 +0100

    forgot to call destroy before invoking the run dialog

--- Alt_Tab_Enhanced@autarkper/extension.js
+++ Alt_Tab_Enhanced@autarkper/extension.js
@@ -582,6 +582,7 @@ AltTabPopup.prototype = {
                 this._finish();
                 return true;
             } else if (action == Meta.KeyBindingAction.PANEL_RUN_DIALOG) {
+                this.destroy();
                 if (this._currentApp >= 0) {
                     let window = this._appIcons[this._currentApp].window;
                     Main.activateWindow(window);
