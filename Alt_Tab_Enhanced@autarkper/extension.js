a691bfa77731a78a94056fc4669bf610cb29b218
Per Ångström <github@autark.se>
2013-03-13 11:55:17 +0100

    only display workspace name if there is more than one workspace

--- Alt_Tab_Enhanced@autarkper/extension.js
+++ Alt_Tab_Enhanced@autarkper/extension.js
@@ -1723,13 +1723,18 @@ ThumbnailHolder.prototype = {
                 let title = new St.Label({text: window.title});
                 label.add(title, { x_fill: false, y_fill: false, y_align: St.Align.MIDDLE });
 
-                let wsString = "[" + Main.getWorkspaceName(window.get_workspace().index()) + "]";
+                let label2strings = [];
+                if (global.screen.n_workspaces > 1) {
+                    label2strings.push("[" + Main.getWorkspaceName(window.get_workspace().index()) + "]");
+                }
                 let windowMonitor = window.get_monitor();
-                let monitorString = windowMonitor != g_myMonitorIndex
-                    ? " (Monitor " + (windowMonitor + 1) + ")"
-                    : '';
-                let label2 = new St.Label({text: wsString + monitorString});
-                label.add(label2, { x_fill: false, y_fill: false, y_align: St.Align.MIDDLE });
+                if (windowMonitor != g_myMonitorIndex) {
+                    label2strings.push("(Monitor " + (windowMonitor + 1) + ")");
+                }
+                if (label2strings.length) {
+                    let label2 = new St.Label({text: label2strings.join(" ")});
+                    label.add(label2, { x_fill: false, y_fill: false, y_align: St.Align.MIDDLE });
+                }
             }
 
             let binHeight = this.actor.allocation.y2 - this.actor.allocation.y1 - headerHeight;
